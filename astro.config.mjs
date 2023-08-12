313254b7a5eef793a506760a592c12da1401bb60
Jesús Mira <jesmira@hotmail.es>
2023-08-12 11:05:37 +0200

    astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://jesusmira.github.io',
+  base: '/astro-landing-curso',
 });
